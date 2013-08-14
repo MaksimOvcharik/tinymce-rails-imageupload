7197d219cbc9a56ec58c2f689ed459e04cbd0ac8
Maksim Ovcharik <maksim.ovcharik@gmail.com>
2013-08-14 21:59:35 +0600

    fix upload

--- app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
+++ app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
@@ -7,7 +7,7 @@
       function showDialog() {
         this.win = editor.windowManager.open({
           width:  350 + parseInt(editor.getLang('uploadimage.delta_width', 0), 10),
-          height: 180 + parseInt(editor.getLang('uploadimage.delta_height', 0), 10),
+          height: 210 + parseInt(editor.getLang('uploadimage.delta_height', 0), 10),
           url: url + '/dialog.html',
         }, {
           plugin_url: url
